14f167d2b13eb6b9401a449ee6d0091a44f4cd7f
andeng07 <sumilangandreijohn@gmail.com>
2025-08-06 22:54:50 +0800

    refactor: rearrange list of colleges alphabetically

--- src/main/kotlin/org/id125/ccs/discord/profile/College.kt
+++ src/main/kotlin/org/id125/ccs/discord/profile/College.kt
@@ -5,10 +5,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 enum class College(val abbreviation: String, val displayName: String, val emoji: String) {
     CCS("CCS", "College of Computer Studies", "💻"),
+    CED("CED", "Br. Andrew Gonzalez College of Education", "📚"),
+    CLA("CLA", "College of Liberal Arts", "🎭"),
     COB("COB", "Ramon V. del Rosario College of Business", "💼"),
     COE("COE", "Gokongwei College of Engineering", "🛠️"),
-    CLA("CLA", "College of Liberal Arts", "🎭"),
     COS("COS", "College of Science", "🔬"),
-    CED("CED", "Br. Andrew Gonzalez College of Education", "📚"),
     SOE("SOE", "School of Economics", "📈");
 }
